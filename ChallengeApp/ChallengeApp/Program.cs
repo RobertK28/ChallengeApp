b59ea813e275de8a14445ccf227848310fc626d3
RobertK28 <robertkmg44@gmail.com>
2023-01-22 17:15:37 +0100

    Zadanie Dzień 4_b

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,7 +1,10 @@
-﻿var name = "Ewa";
+﻿
+
+var name = "Ewa";
 var age = 28;
 bool plecKobieta = true;
 //plecKobieta = false;
+
 
 if (plecKobieta && age < 30)
 {
@@ -16,3 +19,19 @@ else if (!(plecKobieta) && age < 18)
     Console.WriteLine("Niepełnoletni Mężczyzna");
 }
 
+if (plecKobieta)
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poinżej 30 lat");
+    }
+    else if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+}
+else if (!plecKobieta)
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni Mężczyzna");
+    }
