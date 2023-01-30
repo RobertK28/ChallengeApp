2fa27eaf0c941ddf593c765515882bbc08c6b051
RobertK28 <robertkmg44@gmail.com>
2023-01-30 20:09:22 +0100

    Zadanie Dzień 6.

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,40 @@
+﻿namespace ChallengeApp
+{
+    internal class Employee
+    {
+        List<int> pointsOfEmployee = new List<int>();
+       
+        public Employee(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+
+        public int PointsOfEmployee
+        {
+            get
+            {
+                return pointsOfEmployee.Sum();
+            }
+        }
+
+        public string DataOfEmployee 
+        { 
+            get 
+            { 
+                return this.Name + " " + this.Surname + " " + this.Age + " lat,"; 
+            } 
+        }
+              
+        public void AddPointsOfEmployee(int point)
+        { 
+            this.pointsOfEmployee.Add(point);
+        }
+      
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,39 +1,74 @@
-﻿
-using System.Diagnostics.CodeAnalysis;
+﻿using ChallengeApp;
+using System.Runtime.CompilerServices;
 
-int number = 4566;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToCharArray();
-List<char> charsNr = numberInString.ToCharArray().ToList();
+Employee employee1 = new Employee("Tom", "Cruise", 25);
+Employee employee2 = new Employee("Brad", "Pitt", 32);
+Employee employee3 = new Employee("Tomasz", "Kot", 28);
 
+// Points of Employee1
+employee1.AddPointsOfEmployee(1);
+employee1.AddPointsOfEmployee(3);
+employee1.AddPointsOfEmployee(5);
+employee1.AddPointsOfEmployee(6);
+employee1.AddPointsOfEmployee(4);
 
-//int numberRange = 1234567890;
-//string numberRangeInString = numberRange.ToString();
-string numberRangeInString = "0123456789";
-char[] lettersRange = numberRangeInString.ToCharArray();
-List<char> charsRange = new List<char>();
-charsRange = numberRangeInString.ToCharArray().ToList();
+// Points of Employee2
+employee2.AddPointsOfEmployee(3);
+employee2.AddPointsOfEmployee(2);
+employee2.AddPointsOfEmployee(5);
+employee2.AddPointsOfEmployee(7);
+employee2.AddPointsOfEmployee(4);
 
-//Listy
-foreach (char chRange in charsRange)
-{
-    int i = 0; 
-    foreach (char chNr in charsNr)
-    {
-        if (chRange == chNr)
-            i++;
-    }
-    Console.WriteLine(chRange + " => " + i);
-}
+// Points of Employee3
+employee3.AddPointsOfEmployee(4);
+employee3.AddPointsOfEmployee(7);
+employee3.AddPointsOfEmployee(5);
+employee3.AddPointsOfEmployee(6);
+employee3.AddPointsOfEmployee(8);
 
-// Tablice
-foreach (var nrRange in lettersRange)
+var ratingEmployee1 = employee1.PointsOfEmployee;
+var ratingEmployee2 = employee2.PointsOfEmployee;
+var ratingEmployee3 = employee3.PointsOfEmployee;
+
+List<int> ratingListOfEmployee = new List<int> 
+{ ratingEmployee1, ratingEmployee2, ratingEmployee3 };
+
+int maxRating = -1;
+Employee employeeWithMaxRating = null;
+int indexOfratingListOfEmployee = -1;
+Employee ratingEmployeeEach = null;
+
+foreach (var ratingEmployee in ratingListOfEmployee)
 { 
-    int i = 0;
-    foreach (var nr in letters)
+    if (ratingEmployee > maxRating)
     {
-        if (nrRange == nr)
-            i++;
-    }   
-    //Console.WriteLine(nrRange + " => " + i);
+        maxRating = ratingEmployee;
+        indexOfratingListOfEmployee = ratingListOfEmployee.IndexOf(ratingEmployee);
+
+        if (indexOfratingListOfEmployee == 0)
+        {
+            employeeWithMaxRating = employee1;
+            ratingEmployeeEach = employee1;
+        }
+        else if (indexOfratingListOfEmployee == 1)
+        {
+            employeeWithMaxRating = employee2;
+            ratingEmployeeEach = employee2;
+        }
+        else if (indexOfratingListOfEmployee == 2)
+        { 
+            employeeWithMaxRating = employee3;
+            ratingEmployeeEach = employee3;
+        }
+    }
+    Console.WriteLine($"{ratingEmployeeEach.DataOfEmployee} -zdobył {maxRating} punktów.");
+    
 }
+Console.WriteLine("\n");
+
+Console.WriteLine($"Największą liczbę ocen zdobył/a {employeeWithMaxRating.DataOfEmployee} z wynikiem : { maxRating }  punktów.");
+
+
+
+
+
